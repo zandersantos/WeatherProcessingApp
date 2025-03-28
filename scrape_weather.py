c559b187276f775ea70fb688663b61641c81bd9e
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-03-27 22:32:20 -0500

    Added a header comment block that includes important information about the project

---
+++ scrape_weather.py
@@ -0,0 +1,10 @@
+"""
+Name: Zander Santos
+Date: March 25th, 2025
+Description:
+
+- Create a Weather Processing Application that scrapes
+Winnipeg Weather Data (min, max, and mean temperatures) from the Enviornment Canada Website (http://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2018&Month=5).
+
+"""
+
